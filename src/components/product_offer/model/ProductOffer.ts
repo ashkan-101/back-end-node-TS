0bddc557720b04983e57dd9a0cc02a7fe704f049
ashkan-101 <ashkan.dev.101@gmail.com>
2024-08-12 15:38:28 +0330

    added Product_offer

---
+++ src/components/product_offer/model/ProductOffer.ts
@@ -0,0 +1,12 @@
+import { model, Model, Schema } from "mongoose";
+import IProductOffer from "./IProductOffer";
+import productOfferItemSchema from "./ProductOfferItem";
+
+const productOfferSchema: Schema = new Schema({
+  products: {type: [productOfferItemSchema], required: true},
+  start_date: {type: Date, required: true},
+  end_date: {type: Date, required: true}, 
+  created_at: {type: Date, default: Date.now()}
+})
+
+export default model<IProductOffer>('ProductOffer', productOfferSchema)
