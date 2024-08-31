de007d2a7a15443f07d98f7af784c3ad195e7d2e
ashkan-101 <ashkan.dev.101@gmail.com>
2024-08-31 15:28:39 +0330

    added components/payment/model/

---
+++ src/components/payment/model/IPayment.ts
@@ -0,0 +1,14 @@
+import { Document } from "mongoose";
+import PaymentStatus from "./PaymentStatus";
+
+export default interface IPayment extends Document {
+  user: string
+  order: string
+  amount: number
+  method: string[]
+  reserve: string
+  refrence: string
+  createdAt: Date
+  updatedAt: Date
+  status: PaymentStatus
+}
