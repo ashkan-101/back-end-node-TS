811e56272da12f114993d6ad4cf03e1fec9249f7
ashkan-101 <ashkan.dev.101@gmail.com>
2024-09-09 17:38:32 +0330

    added PaymentTransformer and fix PaymentsController

---
+++ src/components/payment/PaymentTransformer.ts
@@ -0,0 +1,51 @@
+import ITransformer from "../contracts/ITransformer";
+import DateService from "../../services/DateService";
+import IPayment from "./model/IPayment";
+
+export default class PaymentTransformer implements ITransformer<IPayment>{
+  private readonly dateService: DateService
+
+  constructor(){
+    this.dateService = new DateService()
+  }
+  
+  public transform(item: IPayment){
+    return {
+      id: item._id,
+      user: this.getUser(item.user),
+      order: this.getOrder(item.order),
+      amount: item.amount,
+      method: item.method,
+      reserve: item.reserve,
+      refrence: item.refrence,
+      createdAt: this.dateService.toPersian(item.createdAt.toUTCString()),
+      updatedAt: this.dateService.toPersian(item.updatedAt.toUTCString()),
+      status: item.status
+    }
+  }
+
+  public collection(items: IPayment[]){
+    return items.map((item: IPayment) => this.transform(item))
+  }
+
+  private getUser(user: any){
+    if(!user){ return null }
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      id: user._id
+    }
+  }
+
+  private getOrder(order: any) {
+    if(!order){ return null }
+    return { 
+      id: order._id,
+      totalPrice: order.totalPrice,
+      finalPrice: order.finalPrice,
+      orderLines: order.orderLines
+    }
+  }
+}
+
--- src/components/payment/PaymentsController.ts
+++ src/components/payment/PaymentsController.ts
@@ -1,17 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 import IPaymentRepository from "./repositories/IPaymentRepository";
 import PaymentMongoRepository from "./repositories/PaymentMongoRepository";
-import Order from "../order/model/Order";
 import IOrderRepository from "../order/repositories/IOrderRepository";
 import OrderMongoRepository from "../order/repositories/OrderMongoRepository";
+import PaymentTransformer from "./PaymentTransformer";
+import ITransformer from "../contracts/ITransformer";
+import IPayment from "./model/IPayment";
 
 class PaymentsController {
   private readonly paymentRepository: IPaymentRepository
   private readonly orderRepository: IOrderRepository
+  private readonly paymentTransformer: ITransformer<IPayment>
 
   constructor(){
     this.paymentRepository = new PaymentMongoRepository()
     this.orderRepository = new OrderMongoRepository()
+    this.paymentTransformer = new PaymentTransformer()
     
     this.index = this.index.bind(this)
     this.create = this.create.bind(this)
@@ -20,30 +24,33 @@ class PaymentsController {
   public async index(req: Request, res: Response, next: NextFunction){
     try {
       const payments = await this.paymentRepository.findMany({}, ['user', 'order'])
-      res.send(payments)
+      const paymentTransform = this.paymentTransformer.collection(payments)
+
+      res.send(paymentTransform)
     } catch (error) {
       next(error)
     }
   }
 
   public async create(req: Request, res: Response, next: NextFunction){
-    const order = await this.orderRepository.findOne(req.body.order)
+    try {
+      const order = await this.orderRepository.findOne(req.body.order)
 
-    const newPaymentParams = {
-      user: req.body.user,
-      order: req.body.order,
-      amount: order?.finalPrice,
-      methos: 'online',
-      reserve: Math.random().toString(16),
-      refrence: Math.random().toString(16),
+      const newPaymentParams = {
+        user: req.body.user,
+        order: req.body.order,
+        amount: order?.finalPrice,
+        methos: 'online',
+        reserve: Math.random().toString(16),
+        refrence: Math.random().toString(16),
+      }
+  
+      const newPayment = await this.paymentRepository.create(newPaymentParams)
+      console.log(newPayment);
+      res.send(newPayment)
+    } catch (error) {
+      next(error)
     }
-
-    const newPayment = await this.paymentRepository.create(newPaymentParams)
-    console.log(newPayment);
-    res.send({
-      success: true,
-      newPayment
-    })
   }
 }
 
